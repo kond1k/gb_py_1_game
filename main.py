58c6004ea0e4ec01b56e6213fea45487622fc66e
kond1k <kond1k12@gmail.com>
2019-08-14 14:34:21 +0300

    Complete Homework

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
+print('Добро пожаловать в игру Азбука!')
 gamer = {'name': input('Как вас зовут?\n'),
          'age': int(input('Сколько тебе лет?\n')),
-         'sex': '',
-         'pet_name': '',
+         'sex': input('Укажите свой пол М или Ж \n'),
+         'pet_name': input('Укажите имя своего питомца \n'),
+         'love': input('Вы любите играть в игры? Да или Нет \n'),
          }
 
+if gamer['love'] == 'Да':
+    gamer['love'] = bool(1)
+if gamer['love'] == 'Нет':
+    gamer['love'] = bool(0)
 if gamer['age'] < 18:
-    if gamer['name'] == 'Вася':
-        print(gamer['name'], 'тубу нельзя играть, потому что ты Вася и молодой')
+    print('Тебе нельзя играть, ты слишком маленький')
+    exit(0)
+elif gamer['age'] > 90:
+    sure = input('Игра для вас может быть утомительной, вы Уверены, что хотите играть? Да или Нет')
+    if sure == 'Нет':
+        print('До свидания', gamer['name'])
+        exit(0)
     else:
-        print('Тебе нельзя играть')
-elif gamer['name'] == 'Петя':
-    print('Ты плохой')
+        sure = input('Вы уверены? Да или Нет')
+        if sure == 'Нет':
+            print('До свидания', gamer['name'])
+            exit(0)
+
 else:
-    print('Добро пожаловать в Игру')
+    print('Добро пожаловать в Игру', gamer['name'])
 
-print('Я могу сосчитать твой возраст')
-
+print('Я могу сказать каких букв Алфавита нет в твоем имени')
 i = 0
-while i <= gamer['age']:
-    print(i)
+
+name = list(gamer['name'].lower())
+while i < 32:
+    count = 0
+    j = 0
+    while j < len(name):
+        if name[j] == chr(1072 + i):
+            count += 1
+        j += 1
+
+    if count == 0:
+        print(chr(1072 + i))
     i += 1
-
-    if i > 22:
-        print('замучился считать')
-        break
-else:
-    print('Сработал else в цикле')
-
-print('А еще я могу произнести имя по буквам')
-
-i = 0
-for char in gamer['name']:
-    i += 1
-    if i == 3:
-        continue
-    print(char)
